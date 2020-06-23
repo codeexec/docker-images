e8d8d2b8c09b6e6710eda79689c1891eabc9d8e7
Krzysztof Kowalczyk <kkowalczyk@gmail.com>
2020-06-23 13:17:23 -0700

    update deno from 1.1 to 1.1.1

--- lang/deno.sh
+++ lang/deno.sh
@@ -4,5 +4,5 @@ set -eux
 # https://github.com/replit/polygott/blob/master/languages/deno.toml
 echo "installing deno"
 
-curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=/usr sh -s v1.1.0
+curl -fsSL https://deno.land/x/install/install.sh | DENO_INSTALL=/usr sh -s v1.1.1
 npm i -g typescript-language-server@0.4.0 typescript@3.9.5
